fac19575af95fd89b3843f9c62ed19e2361c0c1a
Eduardo da Silva <eduardo.silva@gmail.com>
2024-10-14 16:40:18 +0000

    FEAT [COMPANY]: Add serializer tests

--- src/core/company/tests/e2e/company/test_e2e_get.py
+++ src/core/company/tests/e2e/company/test_e2e_get.py
@@ -1,22 +1,9 @@
-# from dataclasses import dataclass
 import json
-
-# from uuid import UUID, uuid4
-# from django.test import override_settings
-# from django.urls import reverse
 from model_bakery import baker
 import pytest
-
-# from rest_framework import status
 from rest_framework.test import APIClient
+
 from core.company.infra.django_app.models import Company
-
-
-# @dataclass
-# class Company:
-#     id: UUID
-#     name: str
-#     trade_name: str
 
 
 # @pytest.fixture
@@ -40,36 +27,22 @@ from core.company.infra.django_app.models import Company
 @pytest.mark.django_db
 class TestListAPI:
     def test_list_categories(self) -> None:
-        baker.make(Company, _quantity=3)
+        created_companies = baker.make(Company, _quantity=3)
 
         url = "/api/companies/"
         response = APIClient().get(url)
 
+        expected_data = [
+            {
+                "id": str(company.id),
+                "name": company.name,
+                "trade_name": company.trade_name,
+                "person_type": company.person_type,
+                "is_active": company.is_active,
+            }
+            for company in created_companies
+        ]
+
         assert response.status_code == 200
         assert len(json.loads(response.content)) == 3
-        # response = APIClient().get(url)
-
-        # expected_data = {
-        #     "data": [
-        #         {
-        #             "id": str(category_documentary.id),
-        #             "name": "Documentary",
-        #             "description": "Documentary description",
-        #             "is_active": True,
-        #         },
-        #         {
-        #             "id": str(category_movie.id),
-        #             "name": "Movie",
-        #             "description": "Movie description",
-        #             "is_active": True,
-        #         },
-        #     ],
-        #     "meta": {
-        #         "current_page": 1,
-        #         "per_page": DEFAULT_PAGINATION_SIZE,
-        #         "total": 2,
-        #     },
-        # }
-
-        # assert response.status_code == status.HTTP_200_OK
-        # assert response.data == expected_data
+        assert json.loads(response.content) == expected_data
