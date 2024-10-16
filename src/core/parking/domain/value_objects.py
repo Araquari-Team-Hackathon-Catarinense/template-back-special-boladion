706c8d15226fc8ca2e3dea981ea3eb13a819414c
RafaelBochi <rafaelbochibarros@gmail.com>
2024-10-15 21:06:44 -0300

    FEAT: Change sector type choices #9

--- src/core/parking/domain/value_objects.py
+++ src/core/parking/domain/value_objects.py
@@ -3,5 +3,5 @@ from enum import StrEnum, unique
 
 @unique
 class SectorType(StrEnum):
-    is_free = "is_free"
-    is_contract = "is_contract"
+    ROTATIVE = "ROTATIVE"
+    CONTRACT = "CONTRACT"
