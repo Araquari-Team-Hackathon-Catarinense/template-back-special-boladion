d725b2dcdc39b1d1f1a8aea88ae1a875c0d53e28
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-10-16 14:39:10 -0300

    FEAT: add settings/urls config for user

--- src/django_project/router.py
+++ src/django_project/router.py
@@ -3,8 +3,10 @@ from rest_framework.routers import DefaultRouter
 from core.company.infra.company_django_app.router import router as company_router
 from core.parking.infra.parking_django_app.router import router as parking_router
 from core.uploader.router import router as uploader_router
+from core.user.infra.user_django_app.router import router as user_router
 
 router = DefaultRouter()
 router.registry.extend(company_router.registry)
 router.registry.extend(parking_router.registry)
 router.registry.extend(uploader_router.registry)
+router.registry.extend(user_router.registry)
--- src/django_project/urls.py
+++ src/django_project/urls.py
@@ -2,10 +2,31 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+from core.user.domain.actions import forget_password, reset_password, validate_token
 
 from .router import router
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("api/forget_password/", forget_password, name="forget_password"),
+    path("api/reset_password/", reset_password, name="reset_password"),
+    path("api/validate_token/", validate_token, name="validate_token"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
