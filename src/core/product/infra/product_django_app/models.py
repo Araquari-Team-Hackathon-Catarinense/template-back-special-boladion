acc93f13c767dda3f9cad03eb173d35455e9ecc8
RafaelBochi <rafaelbochibarros@gmail.com>
2024-10-23 22:45:59 +0000

    FEAT: Create the product model #15

---
+++ src/core/product/infra/product_django_app/models.py
@@ -0,0 +1,22 @@
+import uuid
+
+from django.db import models
+
+from core.company.infra.company_django_app.models import Company
+
+
+class Product(models.Model):
+    id = models.UUIDField(primary_key=True, editable=True, default=uuid.uuid4)
+    description = models.CharField(max_length=200)
+    internal_code = models.CharField(max_length=45)
+    is_active = models.BooleanField(default=True)
+    company = models.ForeignKey(
+        Company, on_delete=models.CASCADE, related_name="products"
+    )
+
+    class Meta:
+        db_table: str = "product"
+        verbose_name_plural: str = "products"
+
+    def __str__(self) -> str:
+        return f"{self.description} ({self.company.name})"
