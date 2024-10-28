d38c5f393c99645f1b8667a8c0557439d5d70bd7
RafaelBochi <rafaelbochibarros@gmail.com>
2024-10-28 23:58:32 +0000

    CHORE: Create populate modalities #33

--- src/core/populate/infra/populate_django_app/management/commands/__init__.py
+++ src/core/populate/infra/populate_django_app/management/commands/__init__.py
@@ -2,3 +2,4 @@ from ._company import populate_companies
 from ._measurement_unit import populate_measurement_units
 from ._packing import populate_packings
 from ._user import populate_users
+from ._vehicle import populate_bodies, populate_modalities
--- src/core/populate/infra/populate_django_app/management/commands/_vehicle.py
+++ src/core/populate/infra/populate_django_app/management/commands/_vehicle.py
@@ -1,5 +1,5 @@
-from core.populate.infra.resources.data_vehicle import bodies_data
-from core.vehicle.infra.vehicle_django_app.models import Body
+from core.populate.infra.resources.data_vehicle import bodies_data, modalities_data
+from core.vehicle.infra.vehicle_django_app.models import Body, Modality
 
 
 def populate_bodies() -> None:
@@ -9,3 +9,12 @@ def populate_bodies() -> None:
     bodies_to_create = [Body(**body) for body in bodies_data]
 
     Body.objects.bulk_create(bodies_to_create)
+
+
+def populate_modalities() -> None:
+    if Modality.objects.exists():
+        return
+
+    modalities_to_create = [Modality(**modality) for modality in modalities_data]
+
+    Modality.objects.bulk_create(modalities_to_create)
