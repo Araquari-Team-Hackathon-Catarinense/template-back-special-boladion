25fe65391e745380eae3910681366a036c19c632
RafaelBochi <rafaelbochibarros@gmail.com>
2024-10-28 23:52:50 +0000

    CHORE: Create the e2e test #33

---
+++ src/core/vehicle/tests/e2e/modality/test_e2e_create.py
@@ -0,0 +1,45 @@
+import uuid
+
+import pytest
+from model_bakery import baker
+from rest_framework.test import APIClient
+
+url = "/api/modalities/"
+
+
+@pytest.mark.django_db
+class TestModalityistAPI:
+    def test_create_a_valid_modality(self) -> None:
+        modality = {
+            "description": "modality 1",
+            "axle": "axle 1",
+        }
+
+        response = APIClient().post(
+            url,
+            {
+                "description": modality["description"],
+                "axle": modality["axle"],
+            },
+        )
+
+        assert response.status_code == 201
+        assert response.json() == {
+            "id": response.json()["id"],
+            "description": modality["description"],
+            "axle": modality["axle"],
+        }
+
+    def test_if_create_a_modality_not_passing_the_description_and_axle(
+        self,
+    ) -> None:
+        modality = {}
+
+        response = APIClient().post(
+            url,
+            modality,
+        )
+
+        assert response.status_code == 400
+        assert "Este campo é obrigatório." in response.json()["description"][0]
+        assert "Este campo é obrigatório." in response.json()["axle"][0]
