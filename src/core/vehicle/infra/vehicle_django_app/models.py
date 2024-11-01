eba468ced3c9aac9d0ce30e37c9cf75a38170681
Hiansdt <vnhiansdt@gmail.com>
2024-11-01 11:24:48 +0000

    CHORE [VEHICLE]: Start vehicle model and create vehicle_type value object #34

--- src/core/vehicle/infra/vehicle_django_app/models.py
+++ src/core/vehicle/infra/vehicle_django_app/models.py
@@ -1,6 +1,9 @@
 import uuid
 
 from django.db import models
+from core.__seedwork__.infra.django_app.models import BaseModel
+
+import vehicle
 
 
 class Body(models.Model):
@@ -20,3 +23,13 @@ class Modality(models.Model):
     class Meta:
         db_table = "modality"
         verbose_name_plural = "modalities"
+
+
+class Vehicle(BaseModel):
+    license = models.CharField(max_length=10)
+    chassis = models.CharField(max_length=45)
+    renavam = models.CharField(max_length=45)
+    axle = models.IntegerField()
+    year = models.IntegerField()
+    gross_weight = integerField()
+    vehicle
