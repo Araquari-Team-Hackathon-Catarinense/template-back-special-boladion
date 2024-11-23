9c36cf4e421742ea5bf1a7ba9fde7f62beee1918
lucasantonete@gmail.com <lucasantonete@gmail.com>
2024-11-23 19:24:33 +0000

    REFACTOR: mv receiver for signals

--- src/core/order/infra/order_django_app/models.py
+++ src/core/order/infra/order_django_app/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models import Q, Sum
-from django.db.models.signals import post_save, pre_save
-from django.dispatch import receiver
 
 from core.__seedwork__.infra.django_app.models import BaseModel
 from core.company.infra.company_django_app.models import Company
@@ -62,8 +59,8 @@ class PurchaseSaleOrder(BaseModel):
     operation_terminal = models.ForeignKey(
         Company, on_delete=models.PROTECT, related_name="operation_terminal_orders"
     )
-    quantity = models.FloatField(blank=True, null=True)
-    balance = models.FloatField(blank=True, null=True)
+    quantity = models.FloatField(blank=True, null=True, default=0.0)
+    balance = models.FloatField(blank=True, null=True, default=0.0)
     operation_type = models.CharField(max_length=255, choices=OPERATION_TYPE_CHOICES)
 
     class Meta:
@@ -93,8 +90,8 @@ class TransportContract(BaseModel):
         Company, on_delete=models.PROTECT, related_name="carrier_contracts"
     )
     purchase_sale_order = models.ForeignKey(PurchaseSaleOrder, on_delete=models.PROTECT)
-    quantity = models.FloatField(blank=True, null=True)
-    balance = models.FloatField(blank=True, null=True)
+    quantity = models.FloatField(blank=True, null=True, default=0.0)
+    balance = models.FloatField(blank=True, null=True, default=0.0)
 
     def __str__(self) -> str:
         return f"{self.purchase_sale_order.company.name} - {self.purchase_sale_order.client.name}, {self.quantity}, {self.balance}"
@@ -104,7 +101,7 @@ class Trip(BaseModel):
     transport_contract = models.ForeignKey(
         TransportContract, on_delete=models.PROTECT, null=True, related_name="trips"
     )
-    quantity = models.FloatField(blank=True, null=True)
+    quantity = models.FloatField(blank=True, null=True, default=0.0)
     date = models.DateField(blank=True, null=True)
     order_number = models.CharField(max_length=45, blank=True, null=True)
     vehicle = models.ForeignKey(
@@ -116,56 +113,3 @@ class Trip(BaseModel):
 
     def __str__(self) -> str:
         return f"{self.transport_contract.purchase_sale_order.company.name} - {self.transport_contract.purchase_sale_order.client.name}, {self.quantity}"
-
-
-@receiver(pre_save, sender=Trip)
-def balance(instance, **kwargs) -> None:  # pylint: disable=unused-argument
-    if instance.deleted_at is None:
-        transport_contract = instance.transport_contract
-        if instance.quantity <= 0 or instance.quantity is None:
-            raise ValueError("A quantidade não pode ser menor ou igual a zero.")
-        else:
-            if (
-                instance.quantity > transport_contract.balance
-                and instance._state.adding
-            ):  # pylint: disable=protected-access
-                raise ValueError(
-                    "A quantidade não pode ser maior que o saldo restante."
-                )
-            elif instance._state.adding is False:  # pylint: disable=protected-access
-                old_trip = Trip.objects.get(id=instance.id)
-                a = transport_contract.balance + old_trip.quantity
-                if instance.quantity > a:
-                    raise ValueError(
-                        "A quantidade não pode ser maior que o saldo restante."
-                    )
-
-            filtered_trips = transport_contract.trips.filter(~Q(id=instance.id))
-
-            if filtered_trips.count() == 0:
-                transport_contract.balance = (
-                    transport_contract.quantity - instance.quantity
-                )
-            else:
-                transport_contract.balance = (
-                    transport_contract.quantity
-                    - instance.quantity
-                    - filtered_trips.aggregate(Sum("quantity"))["quantity__sum"]
-                )
-
-            transport_contract.save()
-
-
-@receiver(post_save, sender=Trip)
-def balance_delete(instance, **kwargs) -> None:  # pylint: disable=unused-argument
-    if instance.deleted_at is not None:
-        transport_contract = instance.transport_contract
-        if transport_contract.trips.count() == 0:
-            transport_contract.balance = transport_contract.quantity
-            transport_contract.save()
-        else:
-            transport_contract.balance = (
-                transport_contract.quantity
-                - transport_contract.trips.aggregate(Sum("quantity"))["quantity__sum"]
-            )
-            transport_contract.save()
